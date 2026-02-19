eace9a17d8c069b03004e15dfc2fa4b987b37843
Alina Kanivetska <alinakaniveckaa@gmail.com>
2026-02-19 16:06:25 +0100

    initial product list app

---
+++ .angular/cache/20.3.15/product-list-app/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,14 @@
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-2HW3YGWF.js";
+import "./chunk-LKBPAEKG.js";
+import "./chunk-WDMUDEB6.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
